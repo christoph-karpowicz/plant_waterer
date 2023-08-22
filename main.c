4698eb733d419a4a637a6aa847c5c680854062c7
Krzysztof Karpowicz <christoph.karpowicz@gmail.com>
2023-08-22 22:41:41 +0200

    Connect a 7 segment double display with two shift registers and initialize them with zeros.

--- main.c
+++ main.c
@@ -4,16 +4,46 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
-#define LED_PORT PORTD
-#define LED_PIN PD6
+#define LED_PORT PORTB
+#define LED_PIN PB4
+
+#define SHIFT_REG_SERIAL_PORT PORTD
+#define SHIFT_REG_SERIAL_PIN PD6
+#define SHIFT_REG_CK_PORT PORTB
+#define SHIFT_REG_SCK_PIN PB0
+#define SHIFT_REG_RCK_PIN PB1
+#define DISPLAY_PINS 16
+
+bool display_pins[DISPLAY_PINS];
 
 void init_led() {
-    DDRD |= _BV(LED_PIN);
+    DDRB |= _BV(LED_PIN);
+}
+
+void init_display() {
+    DDRB |= _BV(SHIFT_REG_SCK_PIN);
+    DDRB |= _BV(SHIFT_REG_RCK_PIN);
+    DDRD |= _BV(SHIFT_REG_SERIAL_PIN);
+    
+    SHIFT_REG_CK_PORT &= ~_BV(SHIFT_REG_RCK_PIN);
+    uint8_t i;
+    for (i = 0; i < DISPLAY_PINS; i++) {
+        SHIFT_REG_CK_PORT &= ~_BV(SHIFT_REG_SCK_PIN);
+        if (display_pins[i]) {
+            SHIFT_REG_SERIAL_PORT |= _BV(SHIFT_REG_SERIAL_PIN);
+        } else {
+            SHIFT_REG_SERIAL_PORT &= ~_BV(SHIFT_REG_SERIAL_PIN);
+        }
+        SHIFT_REG_CK_PORT |= _BV(SHIFT_REG_SCK_PIN);
+    }
+    SHIFT_REG_CK_PORT |= _BV(SHIFT_REG_RCK_PIN);
 }
 
 int main(void) {
     init_led();
+    init_display();
     while (1) {
         LED_PORT &= ~_BV(LED_PIN);
         _delay_ms(1000);
