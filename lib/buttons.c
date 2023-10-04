d77c756faf33e9be8d8c43556bdc2c79d600cf07
Krzysztof Karpowicz <christoph.karpowicz@gmail.com>
2023-10-04 20:04:57 +0200

    Button handling refactoring.

--- lib/buttons.c
+++ lib/buttons.c
@@ -15,35 +15,6 @@ volatile uint8_t blue_button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
 volatile bool red_button_active;
 volatile uint8_t red_button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
 
-// blue button pressed
-ISR(INT0_vect) {
-    if (blue_button_wait_timer >= BUTTON_STANDBY_TIMER_TOP) {
-        handle_button_press(&blue_button_wait_timer, &blue_button_active);
-        if (blue_button_active && red_button_active) {
-            display(DOTS);
-        } else {
-            display_number(++counter1);
-        }
-    }
-}
-
-// red button pressed
-ISR(PCINT_B_vect) {
-    if (red_button_wait_timer >= BUTTON_STANDBY_TIMER_TOP) {
-        handle_button_press(&red_button_wait_timer, &red_button_active);
-        if (blue_button_active && red_button_active) {
-            display(DOTS);
-        } else {
-            display_number(++counter2);
-        }
-    }
-}
-
-ISR(TIMER0_COMPA_vect) {
-    handle_button_timer_interrupt(&blue_button_wait_timer, &blue_button_active);
-    handle_button_timer_interrupt(&red_button_wait_timer, &red_button_active);
-}
-
 void init_buttons() {
     // blue button interrupt
     GIMSK |= _BV(INT0);
@@ -61,29 +32,73 @@ void init_buttons() {
     OCR0A = 10;
 }
 
-void enable_timer_interrupt() {
+static void enable_timer_interrupt() {
     TIMSK |= _BV(OCIE0A);
 }
 
-void disable_timer_interrupt() {
+static void disable_timer_interrupt() {
     TIMSK &= ~_BV(OCIE0A);
 }
 
-void handle_button_press(uint8_t *button_wait_timer, bool *button_active) {
-    enable_timer_interrupt();
-    *button_wait_timer = 0;
-    *button_active = true;
+static void do_red_button_action() {
+    display_number(++counter1);
 }
 
-void handle_button_timer_interrupt(uint8_t *button_wait_timer, bool *button_active) {
+static void do_blue_button_action() {
+    display_number(++counter2);
+}
+
+static void do_both_button_action() {
+    display(DOTS);
+}
+
+static void reset_buttons() {
+    red_button_active = false;
+    blue_button_active = false;
+    blue_button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
+    red_button_wait_timer = BUTTON_STANDBY_TIMER_TOP;
+}
+
+static void handle_button_press(uint8_t *button_wait_timer, bool *button_active) {
+    if (*button_wait_timer >= BUTTON_STANDBY_TIMER_TOP) {
+        enable_timer_interrupt();
+        *button_wait_timer = 0;
+        *button_active = true;
+    }
+}
+
+static void handle_button_timer_interrupt(uint8_t *button_wait_timer, bool *button_active,
+                                          void (*action_func)()) {
     if (!(*button_active)) {
         return;
     }
     (*button_wait_timer)++;
     if (*button_wait_timer >= BUTTON_STANDBY_TIMER_TOP) {
-        *button_active = false;
+        if (blue_button_active && red_button_active) {
+            do_both_button_action();
+            reset_buttons();
+        } else {
+            (*action_func)();
+            *button_active = false;
+        }
         if (!blue_button_active && !red_button_active) {
             disable_timer_interrupt();
         }
     }
+}
+
+// blue button pressed
+ISR(INT0_vect) {
+    handle_button_press(&blue_button_wait_timer, &blue_button_active);
+}
+
+// red button pressed
+ISR(PCINT_B_vect) {
+    handle_button_press(&red_button_wait_timer, &red_button_active);
+}
+
+// button timer interrupt
+ISR(TIMER0_COMPA_vect) {
+    handle_button_timer_interrupt(&blue_button_wait_timer, &blue_button_active, &do_blue_button_action);
+    handle_button_timer_interrupt(&red_button_wait_timer, &red_button_active, &do_red_button_action);
 }
